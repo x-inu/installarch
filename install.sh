239ac9df90138a0e8fe7a0a464166cc887798142
Viko <71472526+VikoFirdausi@users.noreply.github.com>
2025-10-09 06:44:54 +0700

    installer Arch

---
+++ install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# ==============================================
+# Arch Linux Full Setup - Cubiespot + Chris Titus
+# ==============================================
+
+echo "=============================================="
+echo "   SCRIPT FULL INSTALL ARCH + KDE + SERVER     "
+echo "=============================================="
+echo
+
+# ----------------------------------------------
+# 2️⃣  Jalankan Script Server Setup dari Chris Titus Tech
+# ----------------------------------------------
+echo
+echo "Menjalankan script setup server dari Chris Titus Tech..."
+curl -fsSL https://raw.githubusercontent.com/ChrisTitusTech/linutil/main/core/tabs/system-setup/arch/server-setup.sh | sh
+
+# ----------------------------------------------
+# 1️⃣  Jalankan Script Install Arch Desktop
+# ----------------------------------------------
+echo "Menjalankan setup Arch Linux Cubiespot..."
+curl -fsSL https://raw.githubusercontent.com/VikoFirdausi/Install-Arch-Linux/main/arch.sh | sh
+
+# ----------------------------------------------
+# 3️⃣  Selesai
+# ----------------------------------------------
+echo
+echo "=============================================="
+echo "   SEMUA SCRIPT TELAH SELESAI DIJALANKAN!"
+echo "=============================================="
