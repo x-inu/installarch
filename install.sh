ec0d52c3e3fd284e3ec0bcc934de709ae1ad8529
Viko <71472526+x-inu@users.noreply.github.com>
2025-10-20 17:16:16 +0700

    Install Arch

--- install.sh
+++ install.sh
@@ -1,40 +1,39 @@
 #!/bin/bash
 
-echo -ne "
-==============================================
-|       SCRIPT FULL INSTALL ARCH LINUX       | 
-==============================================
-"
-# ----------------------------------------------
-# 2️⃣  Jalankan Script Server Setup 
-# ----------------------------------------------
+clear
+echo "=============================================="
+echo "|       SCRIPT FULL INSTALL ARCH LINUX       |"
+echo "=============================================="
 echo
-echo "Menjalankan script setup server"
-curl -fsSL https://raw.githubusercontent.com/x-inu/installarch/refs/heads/main/archserver.sh | sh
 
+PS3=$'\n'"Silakan pilih jenis instalasi (ketik nomor): "
 
-while true; do
-    read -p "Apakah Anda ingin Menginstall Arch Desktop (y/n): " desktop </dev/tty
-    if [ "$desktop" == "y" ]; then
-        # ----------------------------------------------
-        # 1️⃣  Jalankan Script Install Arch Desktop
-        # ----------------------------------------------
-        echo "Menjalankan setup Arch Desktop..."
-        curl -fsSL https://raw.githubusercontent.com/x-inu/installarch/refs/heads/main/archdesktop.sh | sh
-        break
+options=("Install ArchServer" "Install ArchDesktop" "Keluar")
 
-    elif [ "$desktop" == "n" ]; then
-        # ----------------------------------------------
-        # 3️⃣  Selesai
-        # ----------------------------------------------
-        echo -ne "
-   ==============================================
-   |     SELESAI ARCHLINUX TELAH TERINSTALL     | 
-   ==============================================
-   "
-        break
-
-    else
-        echo "Input tidak valid! Harap masukkan 'y' atau 'n'."
-    fi
+select opt in "${options[@]}"
+do
+    case $REPLY in
+        1)
+            echo "Menjalankan script setup server..."
+            curl -fsSL raw.githubusercontent.com/x-inu/installarch/refs/heads/main/archserver.sh | sh
+            break
+            ;;
+        2)
+            echo "Menjalankan script setup desktop..."
+            curl -fsSL raw.githubusercontent.com/x-inu/installarch/refs/heads/main/archdesktop.sh | sh
+            break
+            ;;
+        3)
+            echo "Keluar..."
+            exit 0
+            ;;
+        *)
+            echo "Pilihan tidak valid! Harap masukkan nomor 1, 2, atau 3."
+            ;;
+    esac
 done
+
+echo
+echo "=============================================="
+echo "|     SELESAI ARCHLINUX TELAH TERINSTALL     |"
+echo "=============================================="
