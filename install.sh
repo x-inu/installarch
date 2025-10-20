380deef408809d37248dfa89384038294748faf0
Viko <71472526+x-inu@users.noreply.github.com>
2025-10-20 17:57:46 +0700

    Install Arch

--- install.sh
+++ install.sh
@@ -6,32 +6,28 @@ echo "|       SCRIPT FULL INSTALL ARCH LINUX       |"
 echo "=============================================="
 echo
 
-PS3=$'\n'"Silakan pilih jenis instalasi (ketik nomor): "
-
-options=("Install ArchServer" "Install ArchDesktop" "Keluar")
-
-select opt in "${options[@]}"
-do
-    case $REPLY in
-        1)
-            echo "Menjalankan script setup server..."
-            curl -fsSL raw.githubusercontent.com/x-inu/installarch/refs/heads/main/archserver.sh | sh
-            break
-            ;;
-        2)
-            echo "Menjalankan script setup desktop..."
-            curl -fsSL raw.githubusercontent.com/x-inu/installarch/refs/heads/main/archdesktop.sh | sh
-            break
-            ;;
-        3)
-            echo "Keluar..."
-            exit 0
-            ;;
-        *)
-            echo "Pilihan tidak valid! Harap masukkan nomor 1, 2, atau 3."
-            ;;
+while true; do
+    echo "1) Install ArchServer"
+    echo "2) Install ArchDesktop"
+    echo "3) Keluar"
+    read -p "Pilih nomor [1-3]: " choice </dev/tty
+    case "$choice" in
+        1) echo "Menjalankan script setup server..."
+           curl -fsSL raw.githubusercontent.com/x-inu/installarch/refs/heads/main/archserver.sh | sh
+           break
+           ;;
+        2) echo "Menjalankan script setup desktop..."
+           curl -fsSL raw.githubusercontent.com/x-inu/installarch/refs/heads/main/archdesktop.sh | sh
+           break
+           ;;
+        3) echo "Keluar..."
+           exit 0
+           ;;
+        *) echo "Pilihan tidak valid! Harap masukkan nomor 1, 2, atau 3."
+           ;;
     esac
 done
+
 
 echo
 echo "=============================================="
